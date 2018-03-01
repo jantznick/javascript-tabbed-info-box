92098dd3b336401d95b6aeaa2249f0359534ddb5
Nick Jantz <thenickjantz@gmail.com>
2018-03-01 13:53:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,21 @@
 function main() {
+//Only display first section
     $('.content2').hide();
     $('.content3').hide();
   var setHeight = $('.content1').outerHeight(); 
  $('.absolutefix').css({'height': setHeight+"px" });
-  
-    $('.tab1').on('click', function() {
-      var setHeight = $('.content1').outerHeight(); 
+    
+//On click of tab, hide all tabs and then only show the clicked tab 
+    $('.tab').on('click', function() {
+      var setHeight = $(this).outerHeight(); 
  $('.absolutefix').css({'height': setHeight+"px" });
       
-       $('.content1').show();
-       $('.content2').hide();
-       $('.content3').hide();
-    });
-  
-  $('.tab2').on('click', function() {
-    var setHeight = $('.content2').outerHeight(); 
- $('.absolutefix').css({'height': setHeight+"px" });
-    
-       $('.content2').show();
-       $('.content1').hide();
-       $('.content3').hide();
-    });
- 
-  $('.tab3').on('click', function() {
-    var setHeight = $('.content3').outerHeight(); 
- $('.absolutefix').css({'height': setHeight+"px" });
-    
-       $('.content3').show();
        $('.content1').hide();
        $('.content2').hide();
+       $('.content3').hide();
+       $(this).show();
     });
+
   
 }
 
